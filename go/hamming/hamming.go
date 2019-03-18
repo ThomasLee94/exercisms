a64cafa87ce25bfeedb62d2f1dad27f2932048e2
Thomas Lee <thomaslee9474@gmail.com>
2019-03-17 23:03:23 -0700

    [added] hamming

---
+++ go/hamming/hamming.go
@@ -0,0 +1,11 @@
+package hamming
+
+func Distance(a, b string) (int, error) {
+	// COMPARE STRING A AND B
+	// KEEP COUNTER OF NUMBER OF THINGS THAT DO NOT MATCH
+	// RETURN COUNTER	
+	counter := 0 
+	if len(a) != len(b){
+		err := errors.New() 
+	}
+}

f26960a8b67926f4a8e3efc65a2ad0c6e76897ab
Thomas Lee <thomaslee9474@gmail.com>
2019-03-30 18:02:22 -0700

    [added] hamming

--- go/hamming/hamming.go
+++ go/hamming/hamming.go
@@ -1,11 +1,24 @@
 package hamming
 
+import(
+	"errors"
+)
+
 func Distance(a, b string) (int, error) {
-	// COMPARE STRING A AND B
-	// KEEP COUNTER OF NUMBER OF THINGS THAT DO NOT MATCH
-	// RETURN COUNTER	
-	counter := 0 
-	if len(a) != len(b){
-		err := errors.New() 
-	}
+	
+	// COUNT THE NUM OF CHARACTERS THAT DONT MATCH
+		counter := 0
+		// COMPARE THE TWO STRANDS
+		if len(a) != len(b){
+			err := errors.New("length of the two DNA strands do not match")
+			return -1,err
+		}
+
+		for i := 0; i < len(a); i++ {
+			if a[i] != b[i] {
+				counter ++
+			}
+		}
+
+		return counter, nil
 }

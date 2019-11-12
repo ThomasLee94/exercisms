bab1c0a66020ece82df69089b7acda85d86d0fe9
Thomas <thomaslee9474@gmail.com>
2019-11-11 17:12:04 -0800

    [mv] files

--- isogram/isogram.go
+++ isogram/isogram.go
@@ -6,18 +6,26 @@ import (
 
 //TAKES INPUT STRING AND RETURNS A BOOL
 func IsIsogram(word string) bool {
+
 	if word == "" {
 		return true
 	}
+
 	dict := make(map[rune]bool)
+
+	//
 	for _, char := range word {
+		// index, bool of found
 		_, found := dict[unicode.ToUpper(char)]
+
 		if found {
 			return false
 		}
+
 		if unicode.IsLetter(char) {
 			dict[unicode.ToUpper(char)] = false
 		}
+
 	}
 	return true
 }
